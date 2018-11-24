6d10c4d8bdd2169223999473c588f67b70a47589
Dally Wally <dallin.watac.major@gmail.com>
2018-11-24 07:16:04 -0700

    Updates

---
+++ precache-manifest.47fd3b974242f19838f77c371a58dbf6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8739c43cbbd34f4b6ae4",
+    "url": "/mymymadlibz/static/js/runtime~main.8739c43c.js"
+  },
+  {
+    "revision": "9d01435b320a60f55bef",
+    "url": "/mymymadlibz/static/js/main.9d01435b.chunk.js"
+  },
+  {
+    "revision": "f68dcc1a0c0c6e129be0",
+    "url": "/mymymadlibz/static/js/1.f68dcc1a.chunk.js"
+  },
+  {
+    "revision": "9d01435b320a60f55bef",
+    "url": "/mymymadlibz/static/css/main.7c965928.chunk.css"
+  },
+  {
+    "revision": "f367df1eeaf94088cbf8d497b432ca63",
+    "url": "/mymymadlibz/index.html"
+  }
+];
