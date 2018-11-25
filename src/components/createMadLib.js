6f99fd3348d6cedf9e78103d7a8e1154db5c7b03
Dally Wally <dallin.watac.major@gmail.com>
2018-11-25 11:25:59 -0700

    mad minor change to visuals

--- src/components/createMadLib.js
+++ src/components/createMadLib.js
@@ -9,6 +9,8 @@ const MadLibCreator = ({handleInputChange, handleMadLibSubmit, text}) => {
                 <p className='cursive'>{'There once was a <ADJECTIVE> <MYTHICAL BEAST>. Who lived in a <PLACE>...'}</p>
             </div>
             <div className='component'>
+            <h6>Create Mad Lib!</h6>
+            <br/>
             <form>
                 <textarea rows='5' value={text} onChange={e => handleInputChange(e)}/>
                 <button onClick={e => {e.preventDefault(); handleMadLibSubmit()}} className="btn btn-primary">Submit</button>
